7befb5500bd3dce0f5025132fcbbdc7199702a8a
Kishan Miriyala <kishananuraag@gmail.com>
2024-08-03 13:10:30 +0530

    raindrops_conditionals

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,12 @@
+def convert(number):
+    output = ''
+
+    if number%3==0:
+        output += 'Pling'
+    if number%5==0:
+        output += 'Plang'
+    if number%7==0:
+        output += 'Plong'
+
+
+    return output or str(number)
